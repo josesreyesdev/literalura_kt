0c5fe45cae95acb6b01f440975c2a957f5b9fea6
Jose SR <josesanchez09190@gmail.com>
2024-11-11 23:50:29 -0600

    Add project

---
+++ src/main/kotlin/com/jsrdev/literalura/LiteraluraApplication.kt
@@ -0,0 +1,11 @@
+package com.jsrdev.literalura
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LiteraluraApplication
+
+fun main(args: Array<String>) {
+    runApplication<LiteraluraApplication>(*args)
+}
